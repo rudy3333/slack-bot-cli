425c9059af2c419bc1272880198a3c8ea5b8ccc8
rudy3 <mmax3163@gmail.com>
2025-11-14 23:49:09 -0500

    add .env support

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+load_dotenv()
 
 # This sample slack application uses SocketMode
 # For the companion getting started setup guide, 
